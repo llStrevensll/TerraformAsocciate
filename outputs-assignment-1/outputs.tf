51caf75ea772163190ec408eead1c2102fcb79a6
Steven R <anguelr12@hotmail.com>
2020-11-02 20:32:37 -0500

    outputs-arn

---
+++ outputs-assignment-1/outputs.tf
@@ -0,0 +1,15 @@
+output "test_instance_id" {
+    value = aws_instance.test_instance.id
+}
+
+output "test_instance_2_id" {
+    value = aws_instance.test_instance_2.id
+}
+
+output "test_instance_arn" {
+    value = aws_instance.test_instance.arn
+}
+
+output "test_instance_2_arn" {
+    value = aws_instance.test_instance_2.arn
+}
