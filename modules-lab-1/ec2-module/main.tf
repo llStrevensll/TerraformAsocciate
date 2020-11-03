b35abaf9e11b252b85e8a355242ae235840f19f2
Steven R <anguelr12@hotmail.com>
2020-11-03 16:24:29 -0500

    module

---
+++ modules-lab-1/ec2-module/main.tf
@@ -0,0 +1,22 @@
+
+data "aws_ami" "windows" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["801119661308"] # Canonical
+}
+
+
+resource "aws_instance" "example" {
+    ami = data.aws_ami.windows.id
+    instance_type = "t2.micro"
+}
